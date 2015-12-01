2084cca74ecb9387633ca40ac3636f6093edaf7a
fishpepper <fishpepper@gmail.com>
2015-12-01 18:07:04 +0100

    fixed baudrate to 100kbit

--- sbus.h
+++ sbus.h
@@ -15,10 +15,10 @@ void sbus_exit_failsafe(void);
 void sbus_enter_failsafe(void);
 void sbus_uart_set_mode(__xdata union uart_config_t *cfg);
 
-//best match for 1mbit/s = 999755.859375 bit/s
+//best match for 100kbit/s = 99975.5859375 bit/s
 //baudrate = (((256.0 + baud_m)*2.0**baud_e) / (2**28)) * 26000000.0
-#define SBUS_BAUD_E 15
-#define SBUS_BAUD_M 59
+#define SBUS_BAUD_E 11
+#define SBUS_BAUD_M 248
 
 #define SBUS_DATA_LEN 25
 extern __xdata uint8_t sbus_data[SBUS_DATA_LEN];
