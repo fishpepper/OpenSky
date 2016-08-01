b687aba58a96252d6a424d5413ab8391afb18b6d
fishpepper <fishpepper@gmail.com>
2016-08-01 21:16:02 +0200

    adding ppm

--- ppm.c
+++ ppm.c
@@ -30,10 +30,6 @@
 // s   = sync pulse= 0.3ms
 // CH* = channel pulse low for 0.7-1.7ms -> total channel (incl sync) = 1.0-2.0ms
 // total channel duration: 8*2ms = 16ms -> frame filler >= 4ms
-
-//invert ppm polarity?
-#define PPM_INVERTED 1
-
 EXTERNAL_MEMORY volatile uint8_t ppm_output_index;
 EXTERNAL_MEMORY uint16_t ppm_data_ticks[9];
 
@@ -45,61 +41,9 @@ void ppm_init(void){
     for(i = 0; i<8; i++){
         ppm_data_ticks[i] = PPM_US_TO_TICKCOUNT(1000);
     }
-    /*
-    //no int on overflow:
-    OVFIM = 0;
 
-    //set channels to compare interupt:
-    //CH0: off
-    T1CCTL0 = 0;
-    //CH1: off
-    T1CCTL1 = 0;
-    //CH2: toggle pin on cmp match, will generate sync pulses
-    #if PPM_INVERTED
-    //inverted, set on match, clear on zero
-    T1CCTL2 = T1CCTLx_MODE_COMPARE | T1CCTLx_CMP_SETCLR0;
-    #else
-    //non-inverted, clear on match, set on zero
-    T1CCTL2 = T1CCTLx_MODE_COMPARE | T1CCTLx_CMP_CLRSET0;
-    #endif
+    hal_ppm_init();
 
-    //configure peripheral alternative1 for timer 1:
-    //use alt config 1 -> clr flag -> P0_4 = output
-    PERCFG &= ~(PERCFG_T1CFG);
-
-    //USART1 use ALT2 in order to free up P0_4 for peripheral func
-    PERCFG |= PERCFG_U1CFG;
-
-    //select P0_4 for peripheral function
-    //NOTE: make sure to set usart1 to alt2 config!
-    P0SEL |= (1<<4);
-
-    //select P0_4 as output
-    P0DIR |= (1<<4);
-
-    //prescaler = 128
-    //tickspeed = 26MHz / 8 = 3,25MHz (TICKSPD is set in timeout.c!)
-    //1us = 3.25 ticks -> 2ms = 6500 ticks, 4ms = 13000
-    T1CTL = T1CTL_MODE_MODULO | T1CTL_DIV_1;
-
-
-    //ch2 cmp: sync pulse length
-    SET_WORD_LO_FIRST(T1CC2H, T1CC2L, PPM_SYNC_PULS_LEN_TICKS);
-
-    //overflow:
-    SET_WORD_LO_FIRST(T1CC0H, T1CC0L, PPM_US_TO_TICKCOUNT(1000));
-
-    ppm_output_index = 0;
-
-    //clear pending interrupt flags (IRCON is reset by hw)
-    T1CTL &= ~(T1CTL_CH0_IF | T1CTL_CH1_IF | T1CTL_CH2_IF | T1CTL_OVFIF);
-
-    //overflow causes an int -> reload next channel data
-    OVFIM = 1;
-
-    //enable T1 interrups
-    T1IE = 1;
-*/
     debug("ppm: init done\n"); debug_flush();
 }
 
@@ -108,7 +52,7 @@ void ppm_update(EXTERNAL_MEMORY uint16_t *data){
     uint8_t i=0;
     uint16_t val;
     uint16_t eof_frame_duration = PPM_FRAME_LEN;
-    /*
+
     //convert to ticks for timer
     //input is 0..4095, we should map this to 1000..2000us
     //frsky seems to send us*1.5 (~1480...3020) -> divide by 1.5 (=*2/3) to get us
@@ -125,12 +69,12 @@ void ppm_update(EXTERNAL_MEMORY uint16_t *data){
         eof_frame_duration -= val;
 
         //set ppm tick data, disable ints during this:
-        cli();
+        HAL_PPM_ISR_DISABLE();
         ppm_data_ticks[i] = val;
-        sei();
+        HAL_PPM_ISR_ENABLE();
     }
     ppm_data_ticks[8] = eof_frame_duration;
-*/
+
     //debug("ppm: in "); debug_flush();
     //debug_put_uint16(data[0]);
     //debug(" out ");
@@ -143,52 +87,19 @@ void ppm_exit_failsafe(void){
 
     //start from beginning
     ppm_output_index = 0;
-    /*
-    //configure p0_4 as peripheral:
-    P0SEL |= (1<<4);
 
-    //reset counter:
-    SET_WORD_LO_FIRST(T1CNTH, T1CNTL, 0);
-
-    //re enable timer interrupts:
-    OVFIM = 1;
-
-    //disable T1 interrups
-    T1IE = 1;
-    */
+    hal_ppm_failsafe_exit();
 }
 
 void ppm_enter_failsafe(void){
-    /*
-    //disable interrupts
-    OVFIM = 0;
-
-    //disable T1 interrups
-    T1IE = 0;
-
-    //configure p0_4 as normal i/o:
-    P0SEL &= ~(1<<4);
-
-    //set pins to failsafe level:
-    #if PPM_INVERTED
-    //clear on zero -> default is high
-    P0 |= (1<<4);
-    #else
-    //set on zero -> default is low
-    P0 &= ~(1<<4);
-    #endif
-*/
+    hal_ppm_failsafe_enter();
     //debug("ppm: entered FS\n");
 }
-#if 0
+
 //timer1 interrupt, this handles the reloading of the
 //channel data to the timer cmp register
-void ppm_timer1_interrupt(void) __interrupt T1_VECTOR{
+void ppm_isr(void) {
     uint16_t pulse_len = PPM_US_TO_TICKCOUNT(1000);
-
-    //clear pending interrupt flags (IRCON is reset by hw)
-    T1CTL &= ~(T1CTL_CH0_IF | T1CTL_CH1_IF | T1CTL_CH2_IF | T1CTL_OVFIF);
-
 
     //failsafe mode?
     if (failsafe_active){
@@ -211,9 +122,9 @@ void ppm_timer1_interrupt(void) __interrupt T1_VECTOR{
     }
 
     //set overflow cmp value
-    SET_WORD_LO_FIRST(T1CC0H, T1CC0L, pulse_len);
+    HAL_PPM_UPDATE_CCVALUE(pulse_len);
 }
-#endif
+
 
 #endif
 
