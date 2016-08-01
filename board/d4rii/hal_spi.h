6a45942c9da5f5065d76c643064b68919b883dbe
fishpepper <fishpepper@gmail.com>
2016-08-01 21:14:07 +0200

    rename

--- board/d4rii/hal_spi.h
+++ board/d4rii/hal_spi.h
@@ -5,11 +5,11 @@
 #include "delay.h"
 
 void hal_spi_init(void);
-static void hal_spi_gpio_init(void);
-static void hal_spi_mode_init(void);
-static void hal_spi_dma_init(void);
+static void hal_spi_init_gpio(void);
+static void hal_spi_init_mode(void);
+static void hal_spi_init_dma(void);
+static void hal_spi_init_rcc(void);
 static void hal_spi_enable(void);
-static void hal_spi_rcc_init(void);
 void hal_spi_dma_xfer(uint8_t *buffer, uint8_t len);
 #define hal_spi_csn_lo() { CC25XX_SPI_GPIO->BRR  = (CC25XX_SPI_CSN_PIN); delay_us(1); }
 #define hal_spi_csn_hi() { delay_us(1); CC25XX_SPI_GPIO->BSRR = (CC25XX_SPI_CSN_PIN); }
