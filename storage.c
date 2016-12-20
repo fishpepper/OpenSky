bfae8a46becd0448ef9377e8be9f5ff3136167c1
fishpepper <fishpepper@gmail.com>
2016-12-20 20:49:36 +0100

    better fixed id detection

--- storage.c
+++ storage.c
@@ -48,9 +48,16 @@ void storage_init(void){
     debug_put_newline();
 }
 
+static const uint8_t storage_default_hoptable[] =
+                              { 0x01, 0x42, 0x83, 0xC4, 0x1A, 0x5B, 0x9C, 0xDD, 0x33, 0x74, 0xB5, 0x0B,
+                                0x4C, 0x8D, 0xCE, 0x24, 0x65, 0xA6, 0xE7, 0x3D, 0x7E, 0xBF, 0x15, 0x56,
+                                0x97, 0xD8, 0x2E, 0x6F, 0xB0, 0x06, 0x47, 0x88, 0xC9, 0x1F, 0x60, 0xA1,
+                                0xE2, 0x38, 0x79, 0xBA, 0x10, 0x51, 0x92, 0xD3, 0x29, 0x6A, 0xAB };
+
 void storage_read_from_flash(void){
     uint8_t *storage_ptr;
     uint16_t len;
+    uint8_t i;
 
     debug("storage: reading\n"); debug_flush();
     storage_ptr = (uint8_t*)&storage;
@@ -59,27 +66,28 @@ void storage_read_from_flash(void){
     hal_storage_read(storage_ptr, len);
 
 #ifdef FRSKY_USE_FIXED_ID
-    //allow overrid for testing
-    debug("storage: using fixed id. ignoring storage content...\n");
-    {
-        uint8_t i;
-        static const uint8_t tmp[] = { 0x01, 0x42, 0x83, 0xC4, 0x1A, 0x5B, 0x9C, 0xDD, 0x33, 0x74, 0xB5, 0x0B,
-                                       0x4C, 0x8D, 0xCE, 0x24, 0x65, 0xA6, 0xE7, 0x3D, 0x7E, 0xBF, 0x15, 0x56,
-                                       0x97, 0xD8, 0x2E, 0x6F, 0xB0, 0x06, 0x47, 0x88, 0xC9, 0x1F, 0x60, 0xA1,
-                                       0xE2, 0x38, 0x79, 0xBA, 0x10, 0x51, 0x92, 0xD3, 0x29, 0x6A, 0xAB };
+    //allow override for testing
+    if (1) {
+#else
+    //only init with defaults when no valid storage id was found
+    if (storage.version != STORAGE_VERSION_ID){
+#endif
+        debug("storage: init with defaults (txid 0x1668)\n");
 
-        storage.version = 0xAB;
+        storage.version = STORAGE_VERSION_ID;
+
         //hard coded config for debugging:
         storage.frsky_txid[0] = 0x16;
         storage.frsky_txid[1] = 0x68;
         storage.frsky_freq_offset = DEFAULT_FSCAL_VALUE;
 
         for(i=0; i<FRSKY_HOPTABLE_SIZE; i++){
-            storage.frsky_hop_table[i] = tmp[i];
+            storage.frsky_hop_table[i] = storage_default_hoptable[i];
         }
-    }
-#endif
 
+        //store settings
+        storage_write_to_flash();
+    }
 }
 
 void storage_write_to_flash(void){
